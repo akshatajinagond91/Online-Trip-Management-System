f3459274a96014fb817c04b2f267cb8da1312f12
MayankDagar77 <mayank8762@gmail.com>
2022-11-09 19:46:54 +0530

    Update Day1

---
+++ OnlineTripManagementSystemApp/src/main/java/com/masai/model/Admin.java
@@ -0,0 +1,24 @@
+package com.masai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int adminId;
+	private String adminName;
+	private String password;
+	private String email;
+	private String mobile;
+	
+	public Admin() {
+		
+	}
+	
+	
+}
