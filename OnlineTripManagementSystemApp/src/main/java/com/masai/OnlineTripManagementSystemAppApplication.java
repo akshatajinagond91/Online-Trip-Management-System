f126656f8397acd6d94e2f72e1fdc04493667c8a
Your Name <mayank8762@gmail.com>
2022-11-14 01:40:58 +0530

    Updates

--- OnlineTripManagementSystemApp/src/main/java/com/masai/OnlineTripManagementSystemAppApplication.java
+++ OnlineTripManagementSystemApp/src/main/java/com/masai/OnlineTripManagementSystemAppApplication.java
@@ -27,7 +27,7 @@ public class OnlineTripManagementSystemAppApplication {
 	}
 
 	private ApiInfo apiDetails() {
-		return new ApiInfo("Bus Reservation Hybrid System", "Sample api", "1.0", "Freee",
+		return new ApiInfo("Online Trip Management System", "Sample api", "1.0", "Freee",
 				new springfox.documentation.service.Contact("hilarious group", "#", "a@b.com"), "API l",
 				"http://javabrains.io", Collections.emptyList());
 	}
